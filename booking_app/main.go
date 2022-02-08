cf6f798c70c156a588fd7aba31e5b5b8ca19c527
Kaczkowski Maciej <Maciej.Kaczkowski@orange.com>
2022-02-08 11:23:35 +0100

    begin work on tutorial booking app; timestamp is 1:01:30

---
+++ booking_app/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+
+func main () {
+	conference_name := "Go Conference"
+	// above line is syntactic sugar equivalent of 
+	// var conference_name = "Go Conference"
+	const conference_tickets uint = 50
+	var remaining_tickets uint = 50
+
+	fmt.Printf("conference_tickets is %T, remaining_tickets is %T, conference_name is %T\n", conference_tickets, remaining_tickets, conference_name)
+
+	fmt.Printf("Welcome to %v booking application\n", conference_name)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conference_tickets, remaining_tickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var user_first_name string
+	var user_last_name string
+	var email string
+	var user_tickets uint
+	var bookings [50]string
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&user_first_name)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&user_last_name)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter how many tickets you would like to buy: ")
+	fmt.Scan(&user_tickets)
+
+	remaining_tickets = remaining_tickets - user_tickets
+	fmt.Printf("Thank you %v %v for booking %v tickets. \nYou will receive confirmation via email at %v\n", user_first_name, user_last_name, user_tickets, email)
+	fmt.Printf("%v tickets remainig for %v\n", remaining_tickets, conference_name)
+}
