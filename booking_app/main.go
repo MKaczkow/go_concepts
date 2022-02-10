2211a4b2a3128452a3a3d3f21685806c430464a2
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2022-02-10 12:22:50 +0100

    basic and not impeacable user input

--- booking_app/main.go
+++ booking_app/main.go
@@ -28,6 +28,7 @@ func main () {
 	// var bookings [50] string <-- Array
 
 	for {
+		// validate user input
 		fmt.Println("Enter your first name: ")
 		fmt.Scan(&user_first_name)
 
@@ -39,29 +40,43 @@ func main () {
 
 		fmt.Println("Enter how many tickets you would like to buy: ")
 		fmt.Scan(&user_tickets)
-		
-		if user_tickets > remaining_tickets {
-			fmt.Printf("We only have %v tickets remaining\n", remaining_tickets)
+
+		name_is_valid := len(user_first_name) >= 2 && len(user_last_name) >= 2
+		email_is_valid := strings.Contains(email, "@")
+		num_tickets_is_valid := user_tickets > 0 && user_tickets <= remaining_tickets
+
+		if name_is_valid && email_is_valid && num_tickets_is_valid {
+
+			remaining_tickets = remaining_tickets - user_tickets
+			bookings = append(bookings, user_first_name + " " + user_last_name)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. \nYou will receive confirmation via email at %v\n", user_first_name, user_last_name, user_tickets, email)
+			fmt.Printf("%v tickets remainig for %v\n", remaining_tickets, conference_name)
+			
+			first_names := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				first_names = append(first_names, names[0])
+			}
+
+			fmt.Printf("These are all our bookings: %v\n", first_names)
+
+			if remaining_tickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year")
+				break
+			}
+		} else { 
+			if !name_is_valid {
+				fmt.Println("firt name or last name you entered is too short")
+			}
+			if !email_is_valid {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !num_tickets_is_valid {
+				fmt.Println("number of tickets you entered is invalid")
+			}
 			continue
 		}
 
-		remaining_tickets = remaining_tickets - user_tickets
-		bookings = append(bookings, user_first_name + " " + user_last_name)
-
-		fmt.Printf("Thank you %v %v for booking %v tickets. \nYou will receive confirmation via email at %v\n", user_first_name, user_last_name, user_tickets, email)
-		fmt.Printf("%v tickets remainig for %v\n", remaining_tickets, conference_name)
-		
-		first_names := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			first_names = append(first_names, names[0])
-		}
-
-		fmt.Printf("These are all our bookings: %v\n", first_names)
-
-		if remaining_tickets == 0 {
-			fmt.Println("Our conference is booked out. Come back next year")
-			break
-		}
 	}
 }
