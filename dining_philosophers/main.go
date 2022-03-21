c9f197a4e14bdc49ef8e77e6a0fda6ee5132782c
Kaczkowski Maciej <Maciej.Kaczkowski@orange.com>
2022-03-21 16:56:27 +0100

    began working on dining philosophers

---
+++ dining_philosophers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Table is set...")
+	fmt.Println("Philosophers start to think and they are getting hungry...")
+
+	// set up N (N==5 at first) philosophers
+
+	// start randomly generating their "hunger"
+
+	// create verbose description of what's happening
+
+	// allow graceful stop
+
+	// allow parametrization
+}
