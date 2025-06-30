1874d04879ad46f3881e85ca341629d10e90a446
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2025-06-30 21:10:38 +0200

    WIP: implement symbol table with mock 'Define' and 'Resolve' methods; TEST: add tests for global let statements (failing for now); 119/294 @ pdf compiler book

---
+++ monkey/compiler/compiler/symbol_table.go
@@ -0,0 +1,43 @@
+package compiler
+
+type SymbolScope string
+
+const (
+	GlobalScope SymbolScope = "GLOBAL"
+)
+
+type Symbol struct {
+	Name  string
+	Scope SymbolScope
+	Index int
+}
+
+// Symbol table is a data structure used in interpreters and compilers
+// to associate identifiers with information (symbols).
+// Common 2 methods on a symbol table are:
+// - Define()
+// - Resolve()
+//
+// We will use it to associate identifiers with:
+// - scope
+// - unique number
+//
+// Reference:
+// https://en.wikipedia.org/wiki/Symbol_table
+type SymbolTable struct {
+	store          map[string]Symbol
+	numDefinitions int
+}
+
+func NewSymbolTable() *SymbolTable {
+	s := make(map[string]Symbol)
+	return &SymbolTable{store: s}
+}
+
+func (st *SymbolTable) Define(name string) Symbol {
+	return Symbol{}
+}
+
+func (st *SymbolTable) Resolve(name string) (Symbol, bool) {
+	return Symbol{}, false
+}
