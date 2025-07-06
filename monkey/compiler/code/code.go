77b3939f37fc2f3ca234cac467219eeb16c1b8b1
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2025-07-06 22:10:26 +0200

    FEAT: add support for hash literals in compiler and VM; TEST: implement hash literal tests; 143/284 @ pdf compiler

--- monkey/compiler/code/code.go
+++ monkey/compiler/code/code.go
@@ -40,6 +40,7 @@ const (
 	OpSetGlobal
 	// Composite types
 	OpArray
+	OpHash
 )
 
 type Definition struct {
@@ -72,6 +73,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal: {"OpGetGlobal", []int{2}},
 	OpSetGlobal: {"OpSetGlobal", []int{2}},
 	OpArray:     {"OpArray", []int{2}},
+	OpHash:      {"OpHash", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
